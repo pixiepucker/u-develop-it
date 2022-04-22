55e0715a484536e165ce44a50d1f1a20b9a0d50b
pixiepucker <autumnoffic@gmail.com>
2022-04-22 15:29:25 -0500

    edited get routes

--- server.js
+++ server.js
@@ -25,7 +25,12 @@ const db = mysql.createConnection(
 
 //candidates table information
 app.get('/api/candidates',(req,res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name 
+    AS party_name 
+    FROM candidates 
+    LEFT JOIN parties 
+    ON candidates.party_id = parties.id`;
+
     db.query(sql, (err,rows) => {
         if (err) {
             res.status(500).json({
@@ -43,7 +48,12 @@ app.get('/api/candidates',(req,res) => {
 
 //single candidate statement
 app.get('/api/candidate/:id', (req,res) => {
-    const sql = `SELECT * FROM candidates WHERE id =?`;
+    const sql = `SELECT candidates.*, parties.name 
+    AS party_name 
+    FROM candidates 
+    LEFT JOIN parties 
+    ON candidates.party_id = parties.id 
+    WHERE candidates.id = ?`;
     const params = [req.params.id];
 
     db.query(sql, params, (err, row) => {
