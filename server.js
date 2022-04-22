062c8e93e67486e6a7706072c349689918d7b395
pixiepucker <autumnoffic@gmail.com>
2022-04-22 03:07:02 -0500

    added dq queries for all data and singular data grabs

--- server.js
+++ server.js
@@ -23,17 +23,41 @@ const db = mysql.createConnection(
 );
 
 //candidates table information
-// db.query(`SELECT * FROM candidates`, (err,rows) => {
-//     console.log(rows);
-// });
+app.get('/api/candidates',(req,res) => {
+    const sql = `SELECT * FROM candidates`;
+    db.query(sql, (err,rows) => {
+        if (err) {
+            res.status(500).json({
+                error: err.message
+            });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
 
 //single candidate statement
-// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(row);
-// });
+app.get('/api/candidate/:id', (req,res) => {
+    const sql = `SELECT * FROM candidates WHERE id =?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({
+                error: err.message
+            });
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
 
 //delete a candidate
 // db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err,result) => {
@@ -44,16 +68,16 @@ const db = mysql.createConnection(
 // });
 
 //create a candidate
-const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-            VALUES (?,?,?,?)`;
-const params = [1, 'Ronald', 'Firbank', 1];
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//             VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
 
-db.query(sql, params, (err,result) => {
-    if (err) {
-        console.log(err);
-    }
-    console.log(result);
-});
+// db.query(sql, params, (err,result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
 
 //2nd to LAST: catchall response for any other request (NOT FOUND)
 app.get((req,res) => {
