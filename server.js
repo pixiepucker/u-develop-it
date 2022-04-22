18944d20b6d3164af54f971e31921260376c8db2
pixiepucker <autumnoffic@gmail.com>
2022-04-22 03:26:37 -0500

    added CRUD routes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql2');
 const express = require('express');
 const { contentType } = require('express/lib/response');
+const inputCheck = require('./utils/inputCheck');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -84,16 +85,32 @@ app.delete('/api/candidate/:id', (req,res) => {
 });
 
 //create a candidate
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-//             VALUES (?,?,?,?)`;
-// const params = [1, 'Ronald', 'Firbank', 1];
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+        res.status(400).json({
+            error: errors
+        });
+        return;
+    }
 
-// db.query(sql, params, (err,result) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(result);
-// });
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+    VALUES (?,?,?)`;
+  const params = [body.first_name, body.last_name, body.industry_connected];
+  
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ 
+          error: err.message 
+        });
+      return;
+    }
+    res.json({
+      message: 'insertion successful',
+      data: body
+    });
+  });
+});
 
 //2nd to LAST: catchall response for any other request (NOT FOUND)
 app.get((req,res) => {
