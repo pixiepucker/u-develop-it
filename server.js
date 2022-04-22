977675006f0a4d96deb71ab444746fbec8a55498
pixiepucker <autumnoffic@gmail.com>
2022-04-22 02:42:08 -0500

    added connection to db

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -6,6 +7,19 @@ const app = express();
 //express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: '',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 //2nd to LAST: default response for any other request (NOT FOUND)
 app.get((req,res) => {
